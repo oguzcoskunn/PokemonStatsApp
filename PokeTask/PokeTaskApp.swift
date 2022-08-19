adf87d9588c68d36eb9073cb5df3b7da1df1df6b
Oguz Coskun <oguzcoskunn0@gmail.com>
2022-08-19 21:32:32 +0300

    Initial Commit

---
+++ PokeTask/PokeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTaskApp.swift
+//  PokeTask
+//
+//  Created by Oğuz Coşkun on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
